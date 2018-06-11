247cb2d21ab503295dcb1e1d5399c6e9c54f629f
kattusite <kattusite@gmail.com>
2018-06-11 18:42:26 -0400

    Started adding phoneme selector

--- front/front.js
+++ front/front.js
@@ -1,3 +1,11 @@
+// On load initializer function
+function frontInit() {
+  console.log("Loading page...");
+
+  // Initialize popovers
+  reloadPopovers();
+}
+
 // On click handler for single trait button
 // Hides second trait div
 function handleSingleTrait() {
@@ -20,4 +28,30 @@ function hideElement(element) {
 // Remove the display: none style to the given DOM element
 function unhideElement(element) {
   element.classList.remove("hidden");
+}
+
+// Create and return a new phoneme list DOM element
+function createPhonemeList() {
+  // Create a phoneme input box (an input element)
+
+  // Create a + button to add another phoneme box
+}
+
+// Create a popup in which users can click the phoneme to select/unselect it
+// using bootstrap!
+
+
+// Write one function for each of the input types.
+
+// ie create phonemeSelector
+
+// ie create checkboxes for other traits???
+
+// Reload popovers to ensure proper initialization.
+function reloadPopovers() {
+  // Hide and remove old popover
+  //$("[data-toggle=popover]").popover("dispose");
+
+  // Add new one
+  $("[data-toggle=popover]").popover();
 }
