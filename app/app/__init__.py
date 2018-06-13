65b26f3bbf482f99608c44f0e5827767cb9707be
kattusite <kattusite@gmail.com>
2018-06-13 00:19:52 -0400

    Working flask app

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -1,36 +1,13 @@
 from flask import Flask
-from config import Config
 import os
 # Email error logs
 import logging
 from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
-app.config.from_object(Config)
-
-csrf = CSRFProtect(app)
-
-cas = CAS(app)
-
-bootstrap = Bootstrap(app)
-
-mail = Mail(app)
 
 # Application errors (in production)
-if not app.debug:
-    if app.config['MAIL_SERVER']:
-        auth = None
-        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-        secure = None
-        if app.config['MAIL_USE_TLS']:
-            secure = ()
-        mail_handler = SMTPHandler(
-            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=[app.config['ADMINS'][0]], subject='Princeton Planner Failure',
-            credentials=auth, secure=secure)
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)
+# if not app.debug:
 
-from . import routes, errors
+
+from . import routes#, errors
