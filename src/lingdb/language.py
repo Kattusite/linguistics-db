0419a0745cfed129f906f51967581d607bd1e9c0
Kattusite <Kattusite@users.noreply.github.com>
2023-04-29 13:22:49 -0700

    Add an initial Query implementation

--- src/lingdb/language.py
+++ src/lingdb/language.py
@@ -1,16 +1,20 @@
 """The language module defines a Language and LanguageSet."""
 
+from collections import UserDict
+from collections.abc import Mapping
 import json
-from typing import Any, Dict, Iterable, List, Optional
+from typing import Any, Dict, Iterable, Iterator, List, Optional
 
 
 class Phoneme(str):
-    pass
+    """A Phoneme represents a phoneme in a natural language, plus the glyph used to write it."""
+
 
 class Datapoint:
     """A single named property of a language."""
 
     def __init__(self, key: str, value: Any) -> None:
+        """Initialize a Datapoint."""
         self.key = key
         self.value = value
 
@@ -23,18 +27,30 @@ class Datapoint:
             TYPES = PRIMITIVE | COLLECTION
         """
 
-
     # TODO: This feels like a bridge too far.
 
 
+# TODO: Language is not currently immutable.
+#   It should be a dataclass.
 class Language:
-    """A Language consists of a collection of Datapoints."""
+    """A Language consists of a collection of Datapoints.
+
+    A Language should be treated as immutable.
+    """
 
     def __init__(
         self,
         data: Dict,
     ) -> None:
-        self.name: Optional[str] = data.get('name')
+        """Initialize a Language from the provided data."""
+        name: Optional[str] = data.get('name')
+
+        # Everything will be very painful later on if we allow languages not to have a name.
+        if not name:
+            raise ValueError('A Language must be given a name')
+
+        self.name: str = name
+
         self.student: Optional[str] = data.get('student')
         self.netid: Optional[str] = data.get('netid')
         self.recommend: Optional[str] = data.get('recommend')
@@ -76,45 +92,80 @@ class Language:
         self._data = data
 
     def __getattr__(self, name: str) -> Any:
+        """Get the named attribute of this Language."""
         if name in self._data:
             return self._data[name]
         raise AttributeError(f'{self} has no attribute {name!r}')
 
     def __hash__(self) -> int:
+        """Return a hash of this Language."""
         return hash((self.name, self.student, self.netid))
 
     def __eq__(self, other: object) -> bool:
+        """Return True if this Language is equal to `other`."""
+        # TODO: This is a pretty quick and dirty check, but it works for now.
         return hash(self) == hash(other)
 
     def __str__(self):
+        """Return a string representation of this Language."""
         return f'<Language {self.name}>'
 
     def __repr__(self):
+        """Return a string representation of this Language."""
         return f'Language({json.dumps(self._data, indent=4, ensure_ascii=False)})'
 
 
+class LanguageSet(UserDict, Mapping[str, Language]):
+    """A collection of several Language objects.
 
-class LanguageSet:
-    """A collection of several Language objects."""
+    The collection should be treated as immutable.
+    It behaves like an Iterable[Language], but it is also indexable by the language name.
+
+    The collection should not contain duplicates.
+    """
 
     @classmethod
-    def from_json(cls, filename: str) -> 'Language':
-        """Create a new Language"""
+    def from_json(cls, filename: str) -> 'LanguageSet':
+        """Create a new LanguageSet by loading it from a JSON file."""
         with open(filename, 'r', encoding='utf-8') as f:
             languages_data = json.load(f)
         languages = [Language(language_data) for language_data in languages_data]
         return cls(languages)
 
     def __init__(self, languages: Iterable[Language]):
-        self._languages = set(languages)
+        """Initialize a LanguageSet from the provided `languages`."""
+        # We might get an arbitrary iterable. Convert to a list for safety.
+        self._languages = list(languages)
+
+        # Forbid duplicate Language entries
+        if len(set(self._languages)) != len(self._languages):
+            raise ValueError('A LanguageSet may not contain duplicate Language objects')
+
+        # Call the UserDict constructor to create the name -> Language mapping.
+        super().__init__({language.name: language for language in self._languages})
 
     def __repr__(self):
+        """Return a string representation of this LanguageSet."""
         lang_strs = '\n'.join(f'    {lang!r},' for lang in self._languages)
         return f'LanguageSet(\n{lang_strs}\n)'
 
     def __str__(self) -> str:
+        """Return a string representation of this LanguageSet."""
         lang_strs = ', '.join(str(lang) for lang in self._languages)
         return f'<LanguageSet: {{{lang_strs}}}>'
+
+    def __iter__(self) -> Iterator[Language]:
+        """Return an iterator over the languages in this LanguageSet."""
+        return iter(self.values())
+
+    def __setitem__(self, key: str, item: Language) -> None:
+        """Raise an error, because LanguageSet is immutable."""
+        raise NotImplementedError('LanguageSet objects are immutable')
+
+    def __delitem__(self, key: str) -> None:
+        """Raise an error, because LanguageSet is immutable."""
+        raise NotImplementedError('LanguageSet objects are immutable')
+
 
 DATASETS = '../../data/datasets'
 
--- src/lingdb/query.py
+++ src/lingdb/query.py
@@ -1,6 +1,39 @@
 """The query module represents queries that can be applied to sets of languages."""
 
-from transformer import Transformation
+from typing import Collection, List, Union
+
+from .language import Language, LanguageSet
+from .transform import Transformation
+
+
+_PrimitiveContextType = Union[str, int, float, bool]
+
+Context = Union[_PrimitiveContextType, List[_PrimitiveContextType]]
+"""Any type that can be returned in a QueryResult as the Context for a single Language.
+
+Context is a more specific piece of information about a language.
+
+NOTE: An entire Language itself is not a valid kind of context. It must be more specific than that.
+
+Defined by:
+    PRIMITIVE := str, int, float, bool
+    COLLECTION := List[PRIMITIVE]
+    CONTEXT := PRIMITIVE COLLECTION
+"""
+
+ContextSet = Collection[Context]
+"""A collection containing one Context for each Language in a LanguageSet."""
+
+Result = Union[Context, Language]
+"""Either a raw Language, or a piece of Context about a Language."""
+
+# NOTE: In theory, it's redundant to include `Collection[Result]` and `ContextSet`.
+#   But right now List[Language] and `LanguageSet`
+ResultSet = Union[ContextSet, LanguageSet, Collection[Result]]
+"""Either a raw LanguageSet, or a ContextSet with one context for each language in a LanguageSet."""
+
+History = List
+"""A historical collection tracking all past values of an attribute."""
 
 
 class QueryResult:
@@ -12,6 +45,127 @@ class QueryResult:
     or to pull out data points for use in a graph.
     """
 
+    def __init__(self, language_set: LanguageSet) -> None:
+        """Initialize a QueryResult with the provided language_set.
+
+        At first, the result_set and language_set are both just the starting language_set,
+        and there are no initial pieces of context.
+        """
+        # During Query processing, the LanguageSet may be altered, e.g. by filtering.
+        #
+        # We keep track of the past LanguageSets, mostly for debugging.
+        self._language_sets: History[LanguageSet] = [language_set]
+        """The internal history of all past values of self.language_set, exposed for debugging."""
+
+        # During Query processing, `result` is iteratively updated to track the "running total".
+        #
+        # We initialize it to the input LanguageSet, to account for the special case where no
+        # transformations are applied -- in this case the Query is the identity mapping.
+        self._result_sets: History[ResultSet] = [language_set]
+        """An internal history of all past values of self._result, exposed for debugging."""
+
+        # During Query processing, new context entries might be added to the list.
+        # The list starts empty because no context is captured until specifically requested.
+        self.contexts: List[ContextSet] = []
+        """A list of all sets of context extracted during request processing, if any."""
+
+    ############################################################################
+    #                               Properties
+    ############################################################################
+
+    @property
+    def result_set(self):
+        """The current evaluation of the result of this query."""
+        return self._result_sets[-1]
+
+    @result_set.setter
+    def result_set(self, result_set: ResultSet):
+        self._result_sets.append(result_set)
+
+    @property
+    def language_set(self):
+        """The current LanguageSet being considered by this query."""
+        return self._language_sets[-1]
+
+    @language_set.setter
+    def language_set(self, language_set: LanguageSet):
+        self._language_sets.append(language_set)
+
+    ############################################################################
+    #                               Mutators
+    ############################################################################
+
+    def extract_context(self):
+        """Append the current result to context."""
+        # A ContextSet can only be a list of Context, which must be primitives or lists thereof.
+        # A LanguageSet might appear in self.result_set (since it is a valid ResultSet),
+        # but it is not a valid ContextSet.
+        if isinstance(self.result_set, LanguageSet):
+            raise TypeError(
+                'Cannot extract an entire LanguageSet as a ContextSet. '
+                'Try transforming the LanguageSet first.'
+            )
+        self.contexts.append(self.result_set)
+
+    def apply(self, transformation: Transformation[Result, Result]):
+        """Mutate the current result by applying a Transformation."""
+        old_result_set = self.result_set
+        try:
+            new_result_set = list(map(transformation, old_result_set))
+
+        # A Transformation may raise an arbitrary exception, so we can't be more specific.
+        except Exception as exc:
+            raise RuntimeError('Transformation function raised an error') from exc
+
+        self.result_set = new_result_set
+
+    def filter_languages(self):
+        """Filter the current LanguageSet by removing any Language whose current result is False."""
+        old_language_set = self.language_set
+        old_result_set = self.result_set
+        new_languages = []
+
+        for language, result in zip(old_language_set, old_result_set):
+            # NOTE: In theory, we could try casting `result` to `bool` with `bool(result)`.
+            #   But that seems like it could lead to subtle bugs. For now, we just require
+            #   that clients must have a Predicate in the Query chain immediately before this
+            #   to explicitly cast everything to a bool.
+            if not isinstance(result, bool):
+                raise TypeError('\n'.join([
+                    'Filtering is only possible if the most recent results were of type bool',
+                    f'  Language: {language}',
+                    f'  Result: ({type(result)}) {result}',
+                ]))
+
+            if result:
+                new_languages.append(language)
+
+        new_language_set = LanguageSet(new_languages)
+        self.language_set = new_language_set
+
+
+class QueryDirective:
+    """QueryDirective acts a special processor directive, triggering special Query behavior.
+
+    A QueryDirective slots into a Query's Transformation chain, but it is not a Transformation.
+    Instead, it acts as a placeholder in the Transformation chain to signal to the Query
+    that some special behavior should be injected between ordinary Transformations, like extracting
+    context, or filtering the LanguageSet of interest.
+
+    Unlike an ordinary Transformation, there is no notion of transforming inputs to outputs.
+    A QueryDirective will generally not modify the currently evaluated result of the Query.
+    """
+
+
+class ExtractContext(QueryDirective):
+    """ExtractContext is a QueryDirective signaling a Query should extract context."""
+
+
+class FilterLanguageSet(QueryDirective):
+    """FilterLanguageSet is a QueryDirective signaling a Query should filter its LanguageSet."""
+
+
+# spell-checker:ignore ptkbdg
 class Query:
     """A Query is a collection of transformations that can be applied to set of languages.
 
@@ -32,7 +186,51 @@ class Query:
             )
     """
 
-    def __init__(self):
-        pass
+    def __init__(self) -> None:
+        """Initialize an empty query, with no transformations."""
+        self.transformations: List[Union[Transformation, QueryDirective]] = []
 
-    def apply(Transformation[T, V]) ->
+    def apply(self, transformation: Transformation[Result, Result]) -> 'Query':
+        """Apply the provided transformation to this chain.
+
+        Returns:
+            the Query itself, to allow for method chaining.
+        """
+        self.transformations.append(transformation)
+        return self
+
+    def extract_context(self) -> 'Query':
+        """Extract the result of evaluating the Query at this moment, and save it as context."""
+        self.transformations.append(ExtractContext())
+        return self
+
+    def filter(self) -> 'Query':
+        """Filter the LanguageSet being processed to only those which satisfy the latest predicate.
+
+        Raises:
+            TypeError: if the latest Transformation was not a Predicate
+
+        Returns:
+            - the Query itself, to allow for method chaining.
+
+        NOTE: Though `filter()` does technically allow for method chaining,
+            `filter()` is generally expected to be the final filter in the chain.
+        """
+        self.transformations.append(FilterLanguageSet())
+        return self
+
+    def evaluate(self, languages: LanguageSet) -> QueryResult:
+        """Evaluate the full Query and return a QueryResult with the result and all context."""
+        result = QueryResult(languages)
+
+        for transformation in self.transformations:
+            if isinstance(transformation, Transformation):
+                result.apply(transformation)
+
+            if isinstance(transformation, ExtractContext):
+                result.extract_context()
+
+            if isinstance(transformation, FilterLanguageSet):
+                result.filter_languages()
+
+        return result
